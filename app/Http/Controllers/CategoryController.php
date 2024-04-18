a1c1102fc30433c220258d6f954568fe151e823d
Yefersonibarguen <yibarguen@magnetoglobal.com>
2024-04-18 00:24:16 -0500

    Ultimo comnit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -42,7 +42,8 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('categories.show', compact('category'));
+
     }
 
     /**
@@ -50,15 +51,18 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('categories.edit', compact('Category'));     
     }
+
+  
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(CreateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+        return redirect()->route('categories.index')->with('success', 'Category update successfully');
     }
 
     /**
@@ -66,6 +70,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->route('categories.index')->with('succes', 'Category delete successfully', );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController2;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController2;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function (){
-    return view('home');
-});
-
-Route::get('/message', function (){
-    return "Hello World";
-});
-
-Route::get('/message/{name}', function ($name){
-    return "Hello " . $name;
-});
-
-Route::get('/message2/{name?}', function ($name = 'invitado'){
-    return "Hello " . $name;
-});
-
-Route::get('/message3/{name?}', function ($name = 'invitado'){
-    return view('message', ['name' => $name]);
-});
-
-Route::get('/message4/{name}/{nickname?}', function ($name, $nickname= 'invitado'){
-    return view('message2', ['name' => $name, 'nickname' => $nickname]);
-});
-
-Route::get('/post', [PostController2::class, 'index']);
-
 
 Route::resource( 'categories', CategoryController::class);
-
-
-
-
-
+Route::resource('posts', PostController::class);
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
